44b738557f5c9939e0637c6513e60caf09fdd037
Rotem Barnea <rotembarnea@pop-os.lan>
2025-10-31 23:21:57 +0200

    cleanups

--- src/distribution/hernquist.py
+++ src/distribution/hernquist.py
@@ -15,6 +15,19 @@ class Hernquist(Distribution):
     @staticmethod
     @njit
     def calculate_rho(r: FloatOrArray, rho_s: float = 1, Rs: float = 1, Rvir: float = 1) -> FloatOrArray:
+        """Calculate the density (`rho`) at a given radius.
+
+        This method is meant to be overwritten by subclasses. The function gets called by njit parallelized functions and must be njit compatible.
+
+        Parameters:
+            r: The radius at which to calculate the density.
+            rho_s: The scale density.
+            Rs: The scale radius.
+            Rvir: The virial radius.
+
+        Returns:
+            The density at the given radius.
+        """
         rho = rho_s / ((r / Rs) * (1 + (r / Rs)) ** 3) / np.exp((r / Rvir) ** 2)
         if isinstance(r, float):
             return rho[0]
--- src/distribution/nfw.py
+++ src/distribution/nfw.py
@@ -16,11 +16,25 @@ class NFW(Distribution):
     @staticmethod
     @njit
     def calculate_rho(r: FloatOrArray, rho_s: float = 1, Rs: float = 1, Rvir: float = 1) -> FloatOrArray:
+        """Calculate the density (`rho`) at a given radius.
+
+        This method is meant to be overwritten by subclasses. The function gets called by njit parallelized functions and must be njit compatible.
+
+        Parameters:
+            r: The radius at which to calculate the density.
+            rho_s: The scale density.
+            Rs: The scale radius.
+            Rvir: The virial radius.
+
+        Returns:
+            The density at the given radius.
+        """
         rho = rho_s / ((r / Rs) * (1 + (r / Rs)) ** 2) / np.exp((r / Rvir) ** 2)
         if isinstance(r, float):
             return rho[0]
         return rho
 
     def calculate_theoretical_M(self, r: Quantity['length']) -> Quantity['mass']:
+        """Calculate the enclosed mass based on the theoretical density profile (without truncation)."""
         x = self.to_scale(r)
         return 4 * np.pi * self.rho_s * self.Rs**3 * (np.log(1 + x) - x / (1 + x))
