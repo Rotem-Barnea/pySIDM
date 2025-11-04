0002cb96c052afc14f2c1134f322e6f88bcc8292
Rotem Barnea <rotembarnea@pop-os.lan>
2025-11-05 01:11:40 +0200

    Update halo.py

--- src/halo.py
+++ src/halo.py
@@ -596,6 +596,8 @@ class Halo:
         path.mkdir(exist_ok=True, parents=True)
         if keep_last_backup:
             for file in path.glob('*'):
+                if '_backup.' in file.stem:
+                    continue
                 if file.is_dir():
                     shutil.copytree(file, file.with_stem(f'{file.stem}_backup'), dirs_exist_ok=True)
                 else:
