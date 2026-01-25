8854a9978e4fc7f50cc015df831bb6a6187103d4
Rotem Barnea <rotembarnea@MacBook-Air-Rotem.local>
2026-01-25 17:08:57 +0200

    Update nfw.py

--- src/distribution/nfw.py
+++ src/distribution/nfw.py
@@ -174,7 +174,7 @@ class NFW(Distribution):
         #         **kwargs,
         #     )
         elif name == 'Fornax dSph':  # Calculation. Defaults to `c=18` if not provided.
-            assert c is None or isinstance(c, float), 'cannot use Dutton14 to calculate c when calculating from the db'
+            assert c != 'Dutton14', 'cannot use Dutton14 to calculate c when calculating from the db'
             return cls(
                 **{**cls.calculate_from_half_light(**example_db.get_db_parameters(name=name), c=c or 18), **kwargs},
                 name=name,
